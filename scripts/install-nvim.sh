5ec2b8020f98647a03c139cec047c0a0e06f4610
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-24 04:42:49 +0800

    nvim packer setup

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+git clone --depth 1 \
+	https://github.com/wbthomason/packer.nvim \
+	~/.local/share/nvim/site/pack/packer/start/packer.nvim
