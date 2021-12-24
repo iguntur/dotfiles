f04e286be1a39f553a9a84c669a7524baac3a1fa
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-25 02:47:08 +0800

    set float the packer popup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,30 @@
 require('settings.general')
 require('settings.keymaps')
 
--- Plugins
+-- Autocommand that reloads neovim whenever you save the init.lua file
 vim.cmd([[
 	augroup packer_user_config
 		autocmd!
-		autocmd BufWritePost init.lua source <afile> | PackerCompile
+		autocmd BufWritePost init.lua source <afile> | PackerSync
 	augroup end
 ]])
 
+-- Use a protected call so we don't error out on first use
+local ok = pcall(require, 'packer')
+if not ok then
+	return
+end
+
+-- Have packer use a popup window
+require('packer').init({
+	display = {
+		open_fn = function()
+			return require('packer.util').float({ border = 'rounded' })
+		end,
+	},
+})
+
+-- Plugins
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'    -- Packer can manage itself
 
