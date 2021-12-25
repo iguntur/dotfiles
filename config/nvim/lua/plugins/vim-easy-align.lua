3342602d34bfab6caf55c09d0af26da33a22148b
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-25 23:30:58 +0800

    metatweak vim-easy-aslign

--- config/nvim/lua/plugins/vim-easy-align.lua
+++ config/nvim/lua/plugins/vim-easy-align.lua
@@ -1,3 +1,9 @@
 local use = require('packer').use
+local keymap = vim.api.nvim_set_keymap
 
 use 'junegunn/vim-easy-align' -- Text align
+
+
+-- Settings
+keymap('x', 'ga', '<Plug>(EasyAlign)', { silent = true }) -- Start interactive EasyAlign in visual mode (e.g. vipga)
+keymap('n', 'ga', '<Plug>(EasyAlign)', { silent = true }) -- Start interactive EasyAlign for a motion/text object (e.g. gaip)
