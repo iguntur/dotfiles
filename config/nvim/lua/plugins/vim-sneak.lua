13081e2dc9285df05ffc9b5cfd61ddcc40926e7d
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-26 00:31:38 +0800

    describe the keymaps

--- config/nvim/lua/plugins/vim-sneak.lua
+++ config/nvim/lua/plugins/vim-sneak.lua
@@ -6,5 +6,5 @@ use 'justinmk/vim-sneak' -- Jump to any location specified by two characters.
 -- Settings
 vim.g['sneak#label'] = 1
 
-keymap('n', '<leader>s', '<Plug>Sneak_s', { silent = true })
-keymap('n', '<leader>S', '<Plug>Sneak_S', { silent = true })
+keymap('n', '<leader>s', '<Plug>Sneak_s', { silent = true }) -- jump forward
+keymap('n', '<leader>S', '<Plug>Sneak_S', { silent = true }) -- jump backward
