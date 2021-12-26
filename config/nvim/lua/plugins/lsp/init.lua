61dce23185ae5b975515c1280f166116c6c9ab94
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-26 20:27:17 +0800

    move to lsp/init.lua

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -1,7 +1,5 @@
 local M = {}
 
--- local use = require("packer").use
-
 M.setup = function(use)
 	use 'neovim/nvim-lspconfig' -- enable LSP
 	use 'williamboman/nvim-lsp-installer' -- simple to use language server installer
