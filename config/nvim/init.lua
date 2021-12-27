63ab47eefdf27f8202daced771b16f48c877d5c1
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-28 07:10:24 +0800

    metatweak

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,32 +26,28 @@ packer.init({
 
 -- Plugins
 return packer.startup(function(use)
-	use 'wbthomason/packer.nvim'             -- Packer can manage itself
-	use 'nvim-lua/popup.nvim'                -- An implementation of the Popup API from vim in Neovim
-	use 'nvim-lua/plenary.nvim'              -- Useful lua functions used ny lots of plugins
+	use 'wbthomason/packer.nvim'                        -- Packer can manage itself
+	use 'nvim-lua/popup.nvim'                           -- An implementation of the Popup API from vim in Neovim
+	use 'nvim-lua/plenary.nvim'                         -- Useful lua functions used ny lots of plugins
 
 	-- Plugins and Settings
 	require('plugins.welcome').setup(use)               -- Vim welcome screen
 	require('plugins.file-tree-explorer').setup(use)    -- File explorer
 	require('plugins.ack').setup(use)                   -- text searching
 	require('plugins.autopairs').setup(use)             -- autopairs quotes, parens, brackets, etc
-	-- require('plugins.coc').setup(use)                   -- ...
 	require('plugins.colorschemes').setup(use)          -- color schema
 	require('plugins.completions').setup(use)           -- the completion plugins
 	require('plugins.editorconfig').setup(use)          -- editorconfig
 	require('plugins.emmet').setup(use)                 -- emmet for html, xml, etc
-	-- require('plugins.fzf').setup(use)                   -- fzf
 	require('plugins.git-integration').setup(use)       -- git integration
 	require('plugins.icons').setup(use)                 -- ...
-	-- require('plugins.javascript-typescript')            -- ...
-	require('plugins.goyo-limelight')                   -- Hyperfocus-writing in Vim.
+	require('plugins.goyo-limelight').setup(use)        -- Hyperfocus-writing in Vim.
 	require('plugins.lsp').setup(use)                   -- Language Server Protocol (LSP)
 	require('plugins.markdown').setup(use)              -- ...
 	require('plugins.quick-scope').setup(use)           -- ...
 	require('plugins.syntastic').setup(use)             -- ...
 	require('plugins.syntax').setup(use)                -- ...
 	require('plugins.telescope').setup(use)             -- ...
-	-- require('plugins.vim-airline').setup(use)           -- status bar
 	require('plugins.lualine-nvim').setup(use)          -- status bar
 	require('plugins.tab-bar').setup(use)               -- buffer/file opened tab bar
 	require('plugins.vim-better-whitespace').setup(use) -- ...
@@ -64,5 +60,10 @@ return packer.startup(function(use)
 	require('plugins.vim-surround').setup(use)          -- ...
 	require('plugins.vim-which-key').setup(use)         -- show vim keymap
 	require('plugins.treesitter').setup(use)            -- Treesitter
+
+	-- require('plugins.coc').setup(use)                     -- ...
+	-- require('plugins.fzf').setup(use)                     -- fzf
+	-- require('plugins.javascript-typescript')              -- ...
+	-- require('plugins.vim-airline').setup(use)             -- status bar
 
 end)
