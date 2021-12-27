522a4ea1d1bb70edd3009c98f945d5769f869fb3
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-28 07:07:48 +0800

    add and use nightfox colorschemes

--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -3,12 +3,14 @@ local M = {}
 M.setup = function(use)
 	use 'flazz/vim-colorschemes'
 	use 'NLKNguyen/papercolor-theme'
+	use 'sainnhe/gruvbox-material' -- gruvbox-material
+	use 'EdenEast/nightfox.nvim'   -- nightfox, nordfox, dayfox, dawnfox and duskfox
 
-	local colorscheme = 'gruvbox'
+	local colorscheme = 'nightfox'
 	local ok, _       = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
 	if not ok then
-		vim.notify('colorscheme ' .. colorscheme .. 'not installed yet')
+		vim.notify('colorscheme "' .. colorscheme .. '" not installed yet')
 		return
 	end
 end
--- config/nvim/lua/plugins/lualine-nvim.lua
+++ config/nvim/lua/plugins/lualine-nvim.lua
@@ -13,14 +13,14 @@ M.setup = function(use)
 	end
 
 	lualine.setup({
-	  -- options = {
-		-- icons_enabled = true,
-		-- theme = 'auto',
+	  options = {
+		icons_enabled = true,
+		theme = 'nightfox', -- 'auto'
 		-- component_separators = { left = '?', right = '?'},
 		-- section_separators = { left = '?', right = '?'},
 		-- disabled_filetypes = {},
 		-- always_divide_middle = true,
-	  -- },
+	  },
 	  -- sections = {
 		-- lualine_a = {'mode'},
 		-- lualine_b = {'branch', 'diff', 'diagnostics'},
