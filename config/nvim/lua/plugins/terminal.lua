f835ebe7ffc97da11060f94a17d5eddf7e038299
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-29 04:16:47 +0800

    Use Ctrl-T to open terminal

--- config/nvim/lua/plugins/terminal.lua
+++ config/nvim/lua/plugins/terminal.lua
@@ -1,16 +1,19 @@
 local M = {}
+local keymap = vim.api.nvim_set_keymap
 
-M.setup = function (use)
-	use 'akinsho/toggleterm.nvim'
+M.setup = function(use)
+	use('akinsho/toggleterm.nvim')
 
 	-- see: https://github.com/akinsho/toggleterm.nvim
-	-- local ok, term = pcall(require, 'toggleterm')
+	local ok, term = pcall(require, 'toggleterm')
 
-	-- if not ok then
-	-- 	return
-	-- end
+	if not ok then
+		return
+	end
 
-	-- term.setup({})
+	term.setup()
+
+	keymap('n', '<C-t>', ':ToggleTerm direction=float<CR>', { noremap = true, silent = true })
 end
 
 return M
