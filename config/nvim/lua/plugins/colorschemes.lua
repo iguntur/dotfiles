48ece3a92f3ab5e200503cdda27f744a1d33de91
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-29 07:38:47 +0800

    change colorschemes based on treesitter

--- config/nvim/lua/plugins/colorschemes.lua
+++ config/nvim/lua/plugins/colorschemes.lua
@@ -1,13 +1,17 @@
 local M = {}
 
 M.setup = function(use)
-	use 'flazz/vim-colorschemes'
-	use 'NLKNguyen/papercolor-theme'
-	use 'sainnhe/gruvbox-material' -- gruvbox-material
-	use 'EdenEast/nightfox.nvim'   -- nightfox, nordfox, dayfox, dawnfox and duskfox
+	use({ 'ellisonleao/gruvbox.nvim', requires = { 'rktjmp/lush.nvim' } }) -- gruvbox
+	use('EdenEast/nightfox.nvim') -- nightfox, nordfox, dayfox, dawnfox and duskfox
+	use('folke/tokyonight.nvim') -- tokyonight
+	use('shaunsingh/moonlight.nvim') -- moonlight
+	use('Shatur/neovim-ayu') -- ayu-<dark,light,mirage>
+	use('glepnir/zephyr-nvim') -- zephyr
+	use('ray-x/aurora') -- aurora
+	use('RRethy/nvim-base16') -- base-16-<color-name>
 
-	local colorscheme = 'nightfox'
-	local ok, _       = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+	local colorscheme = 'tokyonight'
+	local ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
 	if not ok then
 		vim.notify('colorscheme "' .. colorscheme .. '" not installed yet')
