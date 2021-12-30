8b1ba7867942673060fd3bba60bb2520c2c6abd8
Guntur Poetra <poetra.guntur@gmail.com>
2021-12-30 10:04:49 +0800

    add keymap for hop easymotion

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -1,7 +1,8 @@
 local M = {}
+local keymap = vim.api.nvim_set_keymap
 
-M.setup = function (use)
-	use {
+M.setup = function(use)
+	use({
 		'phaazon/hop.nvim',
 		config = function()
 			local ok, hop = pcall(require, 'hop')
@@ -12,8 +13,11 @@ M.setup = function (use)
 
 			-- you can configure Hop the way you like here; see :h hop-config
 			hop.setup()
-		end
-	}
+		end,
+	})
+
+	keymap('n', '<leader>jw', ':HopWord<CR>', { noremap = true, silent = true })
+	keymap('n', '<leader>jp', ':HopPattern<CR>', { noremap = true, silent = true })
 end
 
 return M
