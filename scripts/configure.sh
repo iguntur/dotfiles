e17f35ba2f10a2330fdc3ce6f6dcaeb8f907132f
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-07 02:17:55 +0800

    chore(scripts): migrate using zi

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,14 +5,9 @@ set -e
 ##
 ## Zinit installer
 ##
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+sh -c "$(curl -fsSL https://git.io/get-zi)" -- -a zunit
 
 ##
 ## Setup FZF auto-completions and key-binding
 ##
 $(brew --prefix)/opt/fzf/install
-
-##
-## Setup VIM
-##
-# vim +PluginInstall +qall
