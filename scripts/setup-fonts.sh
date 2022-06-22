a46c2ee351a642d8b6f173dc22f098f26e031475
Guntur Poetra <poetra.guntur@gmail.com>
2022-06-22 09:41:24 +0800

    chore(scripts): nerd fonts installations

--- scripts/setup-fonts.sh
+++ scripts/setup-fonts.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
-# nerd fonts. see https://github.com/ryanoasis/nerd-fonts
-brew tap homebrew/cask-fonts
-brew update
-brew install --cask font-hack-nerd-font
+# # nerd fonts. see https://github.com/ryanoasis/nerd-fonts
+# brew tap homebrew/cask-fonts
+# brew update
+
+function install_nerd_font() {
+	font_name="$1"
+	echo "brew install --cask font-$font_name-nerd-font"
+	brew install --cask font-$font_name-nerd-font
+}
+
 brew install --cask font-fira-code
-brew install --cask font-fira-mono-nerd-font
 brew install --cask font-mononoki
 brew install --cask font-jetbrains-mono
-brew install --cask font-jetbrains-mono-nerd-font
 
+nerd_fonts=(
+	"fira-code"
+	"fira-mono"
+	"go-mono"
+	"hack"
+	"inconsolata"
+	"jetbrains-mono"
+	"liberation"
+	"meslo-lg"
+	"monofur"
+	"mononoki"
+	"noto"
+	"roboto-mono"
+	"ubuntu"
+	"ubuntu-mono"
+)
+
+for __fontname in ${nerd_fonts[@]}; do
+	install_nerd_font $__fontname
+done
+
+unset __fontname
